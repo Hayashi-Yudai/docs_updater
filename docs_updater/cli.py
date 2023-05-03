3e9de48c01a50cff5dd6a4493abc0020742bb25a
Yudai Hayashi <yudai@wantedly.com>
2023-05-03 16:39:02 +0900

    Implemented first stage

--- docs_updater/cli.py
+++ docs_updater/cli.py
@@ -1,8 +1,13 @@
 import click
+import json
+from langchain.chat_models import AzureChatOpenAI
+from langchain.schema import HumanMessage
 import os
 from pathlib import Path
 import subprocess
 from typing import Optional
+
+from docs_updater.prompts import create_context_prompt, create_filelist_prompt
 
 
 def env_validate(api_type: str):
@@ -10,11 +15,16 @@ def env_validate(api_type: str):
         raise ValueError("OPENAI_API_KEY is not set")
 
     if api_type == "azure":
-        if not os.environ.get("AZURE_API_VERSION"):
-            raise ValueError("AZURE_API_VERSION is not set")
+        if not os.environ.get("OPENAI_API_VERSION"):
+            raise ValueError("OPENAI_API_VERSION is not set")
 
         if not os.environ.get("AZURE_RESOURCE_NAME"):
             raise ValueError("AZURE_RESOURCE_NAME is not set")
+    if api_type not in ["openai", "azure"]:
+        raise ValueError(
+            f"API type '{api_type}' is not supported. "
+            + "Select from 'openai' or 'azure'."
+        )
 
 
 def get_git_diff(
@@ -45,17 +55,44 @@ def get_current_docs(docs_dir: str) -> dict[str, str]:
     return current_docs
 
 
+def choose_updatable_docs(
+    model, docs_dict: dict[str, str], diff: str
+) -> dict[str, str]:
+    # Create the context and filelist prompts
+    context = create_context_prompt(docs_dict)
+    filelist_prompt = create_filelist_prompt(diff)
+
+    file_list_json_str = model(
+        [HumanMessage(content=context), HumanMessage(content=filelist_prompt)]
+    ).content
+
+    return json.loads(file_list_json_str)
+
+
 @click.command()
 @click.option("--repo", default=None, help="The path to the repository.")
 @click.option(
-    "--docs-dir", default="docs", help="The path to documents in the repository."
+    "--docs-dir",
+    default="docs",
+    help="The path to documents in the repository.",
 )
 @click.option(
-    "--api-type", default="openai", help="The API type to use. Defaults to OpenAI."
+    "--api-type",
+    default="openai",
+    help="The API type to use. Defaults to OpenAI.",
 )
-def main(repo: Optional[str], docs_dir: str, api_type: str) -> None:
+@click.option(
+    "--model-name",
+    default="gpt-3.5-turbo",
+    help="The GPT model to use.",
+)
+def main(
+    repo: Optional[str],
+    docs_dir: str,
+    api_type: str,
+    model_name: str,
+) -> None:
     env_validate(api_type)
-
     if not repo:
         repo = os.getcwd()
 
@@ -66,6 +103,11 @@ def main(repo: Optional[str], docs_dir: str, api_type: str) -> None:
     docs_dict = get_current_docs(repo / docs_dir)
 
     click.echo(f"Ditected documents: {list(docs_dict.keys())}")
+
+    model = AzureChatOpenAI(deployment_name=model_name, temperature=0)
+    files = choose_updatable_docs(model, docs_dict, diff)
+
+    click.echo(f"Files to update: {files}")
 
 
 if __name__ == "__main__":
